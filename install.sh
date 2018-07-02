3bfc50a6ef9b400d7ec6d86c676858eb61c16105
Ha3MrX <33704360+Ha3MrX@users.noreply.github.com>
2018-07-03 01:01:16 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ apt-get install hydra -y
 apt-get install figlet -y
 apt-get dist-upgrade
 figlet Tools is Installed
-echo How to use HPAS1369
-echo Command : python2 HPAS1369.py
+echo How to use Ha3MrX
+echo Command : python Ha3Mrx.py
